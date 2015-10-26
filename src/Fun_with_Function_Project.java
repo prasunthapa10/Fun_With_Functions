79f60dc2befd17370fae14621a5802161458ff21
Prasun Thapa <thapap@student.swosu.edu>
2015-10-26 10:24:28 -0500

    First commit

---
+++ src/Fun_with_Function_Project.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author LAB
+ *
+ */
+public class Fun_with_Function_Project {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
